bc45a8a2cb14bdb91ae62dbdbc514a044b5b1dfd
Tristan Van Maren <tm.vanmaren@gmail.com>
2016-12-22 14:56:54 -0700

    :sparkles: add concurrency -- two layer depth to crawl

--- crawler.go
+++ crawler.go
@@ -8,8 +8,19 @@ import (
 
 func main() {
 	urls := []string{"http://google.com", "http://bing.com", "http://duckduckgo.com"}
-	linkMap := scraper.Scrape(urls)
-	for url, links := range linkMap {
-		fmt.Println(url, links)
+	scrapeChan := make(chan map[string][]string)
+	go scraper.Scrape(urls, scrapeChan)
+	linkMap := <-scrapeChan
+	newLinkMap := make(map[string][]string)
+	for _, links := range linkMap {
+		go scraper.Scrape(links, scrapeChan)
+		newMap := <-scrapeChan
+		for url, newLinks := range newMap {
+			newLinkMap[url] = newLinks
+		}
+	}
+	close(scrapeChan)
+	for url, link := range newLinkMap {
+		fmt.Println("OVERALL", url, "CONTAINS LINKS TO", link)
 	}
 }
